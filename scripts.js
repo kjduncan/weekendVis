ec264e574edbcd4b7b64b0293f0a01a7e8188257
kjduncan <katelynduncan@Katelyns-MacBook-Pro.local>
2019-04-26 14:47:26 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbzOE-mtqH-INV_86uOVUuMAUxglaoMJseQRRKrfCfYykt8lVac/exec'
+console.log($form.serialize());
+
+$('#submit-form').on('click', function(e) {
+  console.log($form.serialize());
+  console.log('test');
+  e.preventDefault();
+  //add thinking state to button
+  // $('.button').changeTExt('spinnter');
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serialize(),
+    success: function(x) {
+      //add submitted text to button
+      // $('.form').appendChild('<div>success</div>');
+
+    }
+    //fail state
+  });
+})
